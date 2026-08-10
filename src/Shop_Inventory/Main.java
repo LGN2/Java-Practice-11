e1fe9e9076f35cd257c15f179db4128031ceeea6
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 15:22:19 +0400

    Working on 412

--- src/Shop_Inventory/Main.java
+++ src/Shop_Inventory/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
     static List<Sellable> stock = new ArrayList<>();
 
     public static void main(String[] args) {
