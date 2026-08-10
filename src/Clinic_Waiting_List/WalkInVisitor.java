cbdcc6431505c8836dd92f666956c042dd5463fa
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 16:33:21 +0400

    Working on 413

--- src/Clinic_Waiting_List/WalkInVisitor.java
+++ src/Clinic_Waiting_List/WalkInVisitor.java
@@ -1,4 +1,45 @@
 package Clinic_Waiting_List;
 
-public class WalkInVisitor {
+public class WalkInVisitor extends AttendableBase {
+
+    private int arrivalOrder;
+
+    public WalkInVisitor() {
+    }
+
+    public WalkInVisitor(String name, int age, int arrivalOrder) {
+        super(name, age);
+        setArrivalOrder(arrivalOrder);
+    }
+
+    public int getArrivalOrder() {
+        return arrivalOrder;
+    }
+
+    public void setArrivalOrder(int arrivalOrder) {
+        if (arrivalOrder > 0) {
+            this.arrivalOrder = arrivalOrder;
+        } else {
+            this.arrivalOrder = 1;
+        }
+    }
+
+    @Override
+    public String getPriority() {
+        return "Walk-in";
+    }
+
+    @Override
+    public int getWaitMinutes() {
+        return arrivalOrder * 15;
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.println(
+                "[Walk-in] " + getName()
+                        + " age " + getAge()
+                        + " waits " + getWaitMinutes() + " min"
+        );
+    }
 }
