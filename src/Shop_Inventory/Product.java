70e2f253a61b2b893aea574f7c667298bcbf7291
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 15:11:42 +0400

    Working on 412

--- src/Shop_Inventory/Product.java
+++ src/Shop_Inventory/Product.java
@@ -1,4 +1,43 @@
 package Shop_Inventory;
 
-public class Product {
+
+public class Product extends SellableBase {
+
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int quantity) {
+        super(name, price);
+        setQuantity(quantity);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity >= 1 && quantity <= 100) {
+            this.quantity = quantity;
+        } else {
+            System.out.println("Quantity must be 1 to 100.");
+        }
+    }
+
+    @Override
+    public String getCategory() {
+        return "Product";
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.printf(
+                "[Product] %s %.2f OMR qty %d%n",
+                getName(),
+                getPrice(),
+                getQuantity()
+        );
+    }
 }
+
