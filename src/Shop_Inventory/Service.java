765d820b54890388432a8117902c872e6c83e9dc
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 15:13:18 +0400

    Working on 412

--- src/Shop_Inventory/Service.java
+++ src/Shop_Inventory/Service.java
@@ -1,4 +1,44 @@
 package Shop_Inventory;
 
-public class Service {
+
+public class Service extends SellableBase {
+
+    private int minutes;
+
+    public Service() {
+    }
+
+    public Service(String name, double price, int minutes) {
+        super(name, price);
+        setMinutes(minutes);
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        if (minutes > 0) {
+            this.minutes = minutes;
+        } else {
+            this.minutes = 0;
+        }
+    }
+
+    @Override
+    public String getCategory() {
+        return "Service";
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.printf(
+                "[Service] %s %.2f OMR %d min%n",
+                getName(),
+                getPrice(),
+                getMinutes()
+        );
+    }
 }
+
+
