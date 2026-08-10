91343f8686b20506807b4e44ea18ac33d53d8a99
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 15:07:58 +0400

    Working on 412

--- src/Shop_Inventory/Sellable.java
+++ src/Shop_Inventory/Sellable.java
@@ -2,11 +2,8 @@ package Shop_Inventory;
 
 
 public interface Sellable {
-
     void printAllInfo();
-
     double getPrice();
-
     String getCategory();
 }
 
--- src/Shop_Inventory/SellableBase.java
+++ src/Shop_Inventory/SellableBase.java
@@ -1,4 +1,48 @@
 package Shop_Inventory;
 
-public class SellableBase {
+
+public abstract class SellableBase implements Sellable {
+
+    private String name;
+    private double price;
+
+    public SellableBase() {
+    }
+
+    public SellableBase(String name, double price) {
+        setName(name);
+        setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            System.out.println("Name cannot be empty.");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price >= 0 && price <= 10000) {
+            this.price = price;
+        } else {
+            System.out.println("Price out of range");
+        }
+    }
+
+    @Override
+    public abstract void printAllInfo();
+
+    @Override
+    public abstract String getCategory();
 }
+
+
