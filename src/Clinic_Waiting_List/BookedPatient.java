4344ea6b6fa2123b9160cbae2a4622fb807ebba2
Almajd Alshualiy <almajdalshualiy@icloud.com>
2026-08-10 16:32:43 +0400

    Working on 413

--- src/Clinic_Waiting_List/BookedPatient.java
+++ src/Clinic_Waiting_List/BookedPatient.java
@@ -1,4 +1,38 @@
 package Clinic_Waiting_List;
 
-public class BookedPatient {
+public class BookedPatient extends AttendableBase {
+
+    private String slotTime;
+
+    public BookedPatient() {
+    }
+
+    public BookedPatient(String name, int age, String slotTime) {
+        super(name, age);
+        this.slotTime = slotTime;
+    }
+
+    public String getSlotTime() {
+        return slotTime;
+    }
+
+    @Override
+    public String getPriority() {
+        return "Booked";
+    }
+
+    @Override
+    public int getWaitMinutes() {
+        return 10;
+    }
+
+    @Override
+    public void printAllInfo() {
+        System.out.println(
+                "[Booked] " + getName()
+                        + ", age " + getAge()
+                        + ", slot " + getSlotTime()
+                        + ", waits " + getWaitMinutes() + " min"
+        );
+    }
 }
